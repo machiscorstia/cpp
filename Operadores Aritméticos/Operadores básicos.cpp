c693b7acab2f6fb8f5ee065b6e1d5a8d371ed9ad
andrewzhbj <42502197+andrewzhbj@users.noreply.github.com>
2020-02-21 15:49:51 -0300

    Operaciones

---
+++ Operadores Aritméticos/Operadores básicos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int primerOperando = 16, segundoOperando = 2, resultado = 0;
+	
+	cout << "Suma: " 			<< primerOperando + segundoOperando << endl;
+	cout << "Resta: " 			<< primerOperando - segundoOperando << endl;
+	cout << "Multiplicación: " 	<< primerOperando * segundoOperando << endl;
+	cout << "División: " 		<< primerOperando / segundoOperando << endl;
+	cout << "Modulo: " 			<< primerOperando % segundoOperando << endl;
+
+
+	return 0;
+}
