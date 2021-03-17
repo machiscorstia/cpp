d74020118d27848827857ddeb9b1ec7ad79e3638
Manuel Cabral <cabral.manuel@yandex.com>
2021-03-16 22:50:21 -0300

    direccion de memoria de un array

--- Punteros/array.cpp
+++ Punteros/array.cpp
@@ -6,8 +6,7 @@ int main(){
     int numeros[] = {1, 2, 3};
     int *direccion = numeros;
 
-    for(int i = 0; i < 3; i++)
-        cout << "Direccion: " << direccion++ << endl;
+    for(int i = 0; i < 3; i++) cout << "Direccion: " << direccion++ << endl;
 
     return 0;
 }
