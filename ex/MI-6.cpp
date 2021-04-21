78b9c17735e58d7d8b19030c5da7d111fab28128
Manu <cabral.manuel@yandex.com>
2021-04-21 11:18:39 -0300

    Update MI-6.cpp

--- ex/MI-6.cpp
+++ ex/MI-6.cpp
@@ -4,18 +4,19 @@
 using namespace std;
 
 int main(){
-    int actual_date = 0, first_date = 0, second_date = 0, result = 0;
+    int actual_date = 0, first_date = 0, second_date = 0, first_date_difference = 0, second_date_difference = 0, result = 0;
 
     cin >> actual_date >> first_date >> second_date;
 
-    if(first_date == second_date){
+    first_date_difference = abs(actual_date - first_date);
+    second_date_difference = abs(actual_date - second_date);
+
+    if(first_date_difference == second_date_difference)
         cout << "Son guales" << endl;
-        return 0;
+    else{
+        result = first_date_difference < second_date_difference ? first_date : second_date;
+        cout << "Fecha mas reciente: " << result;
     }
 
-    result = abs(actual_date - first_date) < abs(actual_date - second_date) ? first_date : second_date;
-    
-    cout << "Fecha mas reciente: " << result;
-
     return 0;
-};
+}
